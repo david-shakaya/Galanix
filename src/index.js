648cd28e915b736913e9922761ff4e2dea0f55c6
david-shakaya <devidoz666@gmail.com>
2021-03-01 03:24:01 +0200

    Api render task3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import './sass/main.scss';
 import '../src/task-1/sass/main.scss';
+import './task-2/sass/main.scss';
+import './task-3/js/renderMarkupHomePage';
 import refs from './task-2/js/refs';
 import renderMarkupTask1 from './task-1/js/task-1';
-// import './task-1/js/task-1.js';
 import renderMarkup from './task-2/js/renderMarkup';
 
-import './task-2/sass/main.scss';
-
 refs.linkTask2.addEventListener('click', renderMarkup);
-
-console.log(refs);
---
+++ src/task-3/js/renderMarkupHomePage.js
@@ -0,0 +1,60 @@
+const wrapperContent = document.querySelector('.wrapper-content-js');
+const linkTask3 = document.querySelector('.link-task-3');
+const wrapperTask3 = document.querySelector('.wrapper-task-3');
+
+linkTask3.addEventListener('click', renderMarkupTask3);
+
+function renderMarkupTask3() {
+  wrapperContent.innerHTML = `<div class="container">
+  <form class="search-form" id="search-form">
+    <input
+      type="text"
+      name="query"
+      autocomplete="off"
+      placeholder="Search images..."
+    />
+    <button>Отправить</button>
+  </form>
+  <section class="section-gallery">
+    <ul class="gallery"></ul>
+  </section>
+  <div id="hide"></div>
+</div>`;
+}
+renderMarkupTask3();
+const form = document.querySelector('.search-form');
+form.addEventListener('submit', search);
+
+function search(e) {
+  e.preventDefault();
+
+  let inputValue = e.target.elements.query.value;
+  fetchUniversities(inputValue);
+}
+
+let numberUniver = 0;
+
+function fetchUniversities(query) {
+  fetch(`http://universities.hipolabs.com/search?country=${query}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const mar = data.reduce(
+        (string, univer) =>
+          string +
+          `  
+          <li class="list-item-univer">
+       <span class="list-item-univer-number">${(numberUniver += 1)}</span> <p class="list-item-univer-name">${
+            univer.name
+          }</p>
+        <a class="list-item-univer-link" href="${univer.web_pages[0]}">${
+            univer.web_pages[0]
+          }</a>
+    </li>`,
+        '',
+      );
+      wrapperTask3.innerHTML = `
+      <h2>По вашему запросу найдено ${data.length} университетов</h2>
+        ${mar}`;
+    });
+}
